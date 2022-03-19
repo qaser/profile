3a148bd41ae9b560403de6ac20d41501758a2ab5
qaser <huji@mail.ru>
2022-03-19 23:25:59 +0500

    main: add js animation text of my skills

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,12 @@
-const firstString = document.querySelector('#first-string');
-const secondString = document.querySelector('#second-string');
-const hintString = document.querySelector('.console__hint');
-const firstCursor = document.querySelector('#first-cursor');
-const secondCursor = document.querySelector('#second-cursor');
-
-const firstText = 'Hi,';
-const secondText = "I'm qaser";
-let originText = '';
-
-const delay = 300;
-
-const delayLoop = (fn, delay, data) => {
-    return (x, i) => {
-        setTimeout(() => {
-        fn(x, data);
-        }, i * delay);
-    }
-};
-
-function setTextContent(char, data) {
-    data.text = data.text + char;
-    data.selector.textContent = data.text;
+function scrollFade() {
+    const texts = document.querySelectorAll('.skill-item__description');
+    texts.forEach(text => {
+         const textPosition = text.getBoundingClientRect().top;
+         const windowHeight = window.innerHeight;
+        if (textPosition < windowHeight / 2) {
+            text.classList.add('skill-item__description_active');
+        }
+    })
 }
 
-
-function startAnimation(text, delay, selector) {
-    const arr = Array.from(text);
-    let originText = '';
-    const data = {
-        selector: selector,
-        text: originText
-    }
-    arr.forEach(delayLoop(setTextContent, delay, data))
-}
+window.addEventListener('scroll', scrollFade);
